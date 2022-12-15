fdee75a3b933aa3d4f9d8965e75cf8c8434ef51f
Jyoti <email.jyotisingh13@gmail.com>
2022-12-15 15:58:46 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button =>{
+    button.addEventListener('click',(e) =>{ 
+        switch(e.target.innerText){
+            case 'clr':
+                display.innerText = '';
+                break;
+            case '<-':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0,-1);
+                }
+                break;
+            case 'ans':
+                try{
+                    display.innerText = eval(display.innerText);                    
+                } catch{
+                    display.innerText = 'Error!';
+                }
+
+                break;
+            
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
+
