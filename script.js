f4627f0b414e504b91479f717f0d51f6ff4ac34d
Jyoti <email.jyotisingh13@gmail.com>
2022-12-15 17:35:55 +0530

    completed

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 //your code here
-let display = document.getElementById('display');
+// let display = document.getElementById('display');
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+// let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map(button =>{
-    button.addEventListener('click',(e) =>{ 
-        switch(e.target.innerText){
-            case 'clr':
-                display.innerText = '';
-                break;
-            case 'ans':
-                try{
-                    display.innerText = eval(display.innerText);                    
-                } catch{
-                    display.innerText = 'Error!';
-                }
+// buttons.map(button =>{
+//     button.addEventListener('click',(e) =>{ 
+//         switch(e.target.innerText){
+//             case 'clr':
+//                 display.innerText = '';
+//                 break;
+//             case 'ans':
+//                 try{
+//                     display.innerText = eval(display.innerText);                    
+//                 } catch{
+//                     display.innerText = 'Error!';
+//                 }
 
-                break;
+//                 break;
             
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
-});
+//             default:
+//                 display.innerText += e.target.innerText;
+//         }
+//     });
+// });
+
+let ans1 ="";
+let num ="";
+function add(value){
+    console.log(value);
+    num=num+value;
+    document.getElementById('input').value=num;
+
+}
 
 
+function ans(){
+    ans1=eval(num);
+    document.getElementById('input').value=ans1;
+    ans1="";
+    num="";
+}
+
+function clear(){
+    num="";
+    document.getElementById('input').value="";
+}
